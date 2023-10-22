a7e39082bf3c55ddc4203d3d879e9262c45510f4
Andrew <andrewstaveley14@gmail.com>
2023-10-22 00:36:23 -0400

    c23/31 tests complete

--- index.js
+++ index.js
@@ -9,37 +9,41 @@ const createEmployeeRecord = function(employeeArray) {
         timeOutEvents: []
     }
 };
-
 const createEmployeeRecords = function(employeeRows) {
     return employeeRows.map(function(row) {
-        return createEmployeeRecord(row)
+        return createEmployeeRecord(row);
     });
 };
-
 const createTimeInEvent = function(employee, dateTimeStamp) {
-    const dateTimeStampObjectIn = dateTimeStamp.split(' ')
+    const dateTimeStampObjectIn = dateTimeStamp.split(' ');
     const timeInObject = {
         type: 'TimeIn',
         date: dateTimeStampObjectIn[0],
         hour: parseInt(dateTimeStampObjectIn[1], 10)
     }
-    employee.timeInEvents.push(timeInObject)
-    return employee
-}
-
+    employee.timeInEvents.push(timeInObject);
+    return employee;
+};
 const createTimeOutEvent = function(employee, dateTimeStamp) {
-    const dateTimeStampObjectOut = dateTimeStamp.split(' ')
+    const dateTimeStampObjectOut = dateTimeStamp.split(' ');
     const timeOutObject = {
         type: 'TimeOut',
         date: dateTimeStampObjectOut[0],
         hour: parseInt(dateTimeStampObjectOut[1], 10)
     }
-    employee.timeOutEvents.push(timeOutObject)
-    return employee
-}
-
+    employee.timeOutEvents.push(timeOutObject);
+    return employee;
+};
 const hoursWorkedOnDate = function(employee, date) {
-    for (let punch in employee.timeInEvents.date) {
-        console.log(punch)
-    }
-}
+    const punchIn = employee.timeInEvents.find(function(x) {
+        return x.date === date;
+    })
+
+    const punchOut = employee.timeOutEvents.find(function(y) {
+        return y.date === date;
+    })
+    return (punchOut.hour - punchIn.hour) / 100
+};
+const wagesEarnedOnDate = function() {
+
+};
